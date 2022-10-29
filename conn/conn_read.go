8797d08ff05d4251cc956a8bcea0d93095acfa7e
moqsien <moqsien@moqsiendeMac.local>
2022-10-28 22:42:53 -0700

    read

---
+++ conn/conn_read.go
@@ -0,0 +1,22 @@
+package conn
+
+import "io"
+
+func (that *Conn) Read(p []byte) (n int, err error) {
+	if that.InBuffer.IsEmpty() {
+		n = copy(p, that.Buffer)
+		that.Buffer = that.Buffer[n:]
+		if n == 0 && len(p) > 0 {
+			err = io.EOF
+		}
+		return
+	}
+	n, _ = that.InBuffer.Read(p)
+	if n == len(p) {
+		return
+	}
+	m := copy(p[n:], that.Buffer)
+	n += m
+	that.Buffer = that.Buffer[m:]
+	return
+}
