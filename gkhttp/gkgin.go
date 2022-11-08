8c2bb4b44320212de51b16a8213ffb76813346dd
moqsien <moqsien@foxmail.com>
2022-11-08 14:47:39 +0800

    rewrite Run

--- gkhttp/gkgin.go
+++ gkhttp/gkgin.go
@@ -2,6 +2,7 @@ package gkhttp
 
 import (
 	"net"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 
@@ -29,21 +30,40 @@ func (that *GkGin) initServer() {
 }
 
 func (that *GkGin) Close() {
-	that.Server.Close()
+	if that.Server != nil {
+		that.Server.Close()
+	}
 }
 
 func (that *GkGin) Listen(network, address string) (iface.IListener, error) {
+	that.initServer()
 	return that.Server.Listen(network, address)
 }
 
 func (that *GkGin) AdoptOneListener(ln net.Listener) (iface.IListener, error) {
+	that.initServer()
 	return that.Server.AdoptOneListener(ln)
 }
 
 func (that *GkGin) Serve() error {
+	that.initServer()
 	return that.Server.Serve()
 }
 
 func (that *GkGin) ServeTLS(certFile string, keyFile string) error {
+	that.initServer()
 	return that.Server.ServeTLS(certFile, keyFile)
+}
+
+func (that *GkGin) Run(addr ...string) error {
+	that.initServer()
+	if len(addr) > 0 {
+		if strings.HasSuffix(addr[0], ".sock") {
+			that.Listen("unix", addr[0])
+		} else {
+			that.Listen("tcp", addr[0])
+		}
+		return that.Serve()
+	}
+	return that.Serve()
 }
